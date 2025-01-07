7ae4a555b0d7d6b978b2813f0637e02717d4b671
TomasBankauskas <tbdizainas@gmail.com>
2025-01-07 15:12:20 +0200

    accessibility improvements

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
+<script setup>
+useHead({
+  htmlAttrs: { lang: 'en-US' },
+  title: 'Nuxt Starter',
+  meta: [
+    { name: 'description', content: 'Nuxt Starter is a minimal template created for quick landing pages, built with Nuxt and Tailwind CSS.' }
+  ],
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
